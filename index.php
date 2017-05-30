26a8d16cbfb2c16ada3a24a5d2a155a846bf2fad
Lisbeth Guillén <lisguillen1@gmail.com>
2017-05-29 21:41:45 -0500

    avance del home, sección de servicios

---
+++ index.php
@@ -0,0 +1,130 @@
+<?php include('head.html') ?>
+<?php include('header.html') ?>
+
+
+<section>
+  <div class="container">
+    <div class="titulo center">
+        <h2>Nuestros Servicios</h2>
+    </div>
+    <div class="row">
+      <div class="col s12 m6 l4 xl3">
+        <div class="card">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="img/mesa.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">This is a link</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          </div>
+        </div>      
+      </div>
+      <div class="col s12 m6 l4 xl3">
+        <div class="card">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="img/mesa.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">This is a link</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          </div>
+        </div>      
+      </div>
+      <div class="col s12 m6 l4 xl3">
+        <div class="card">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="img/mesa.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">This is a link</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          </div>
+        </div>      
+      </div>
+      <div class="col s12 m6 l4 xl3">
+        <div class="card">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="img/mesa.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">This is a link</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          </div>
+        </div>      
+      </div>
+    </div>  
+  </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="titulo center">
+            <h2>Otros Servicios</h2>
+        </div>    
+        <div class="row">
+            <div class="col s12">
+            <ul id="tabs-swipe-demo" class="tabs tabs-fixed-width">
+                <li class="tab col s3"><a class="active" href="#tab1">Tab 1</a></li>
+                <li class="tab col s3"><a href="#tab2">Tab 2</a></li>
+                <li class="tab col s3"><a href="#tab3">Tab 3</a></li>
+                <li class="tab col s3"><a href="#tab4">Tab 4</a></li>
+            </ul>
+            </div>
+            <div id="tab1" class="col s12">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div id="tab2" class="col s12">
+                <img src="img/mesa.jpg">
+            </div>
+            <div id="tab3" class="col s12">
+                <h3>Tab 3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div id="tab4" class="col s12">
+                <h3>Tab 4</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>        
+    </div>
+</section>
+
+ 
+
+
+<!-- Librería jQuery requerida por los plugins de JavaScript -->
+<script src="js/jquery.js"></script>
+<script src="js/materialize.min.js"></script>
+
+  </body>
+</html>
